4f55b92e387fadc532c55b811a8ceb474308b85d
vinay joshi <vinay@vinays-MacBook-Air.local>
2024-10-27 14:35:22 -0400

    added basic webpages

---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("",views.home, name="home"),
+    path("room/",views.room, name="room"),
+    path("safety/",views.safety,name="safety"),
+    path("about/",views.about,name="about"),
+    path("support/",views.support,name="support"),
+]
---
+++ base/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+
+# these are the html links to the navbar about, support and safety.
+
+def about(request):
+    return render(request,'navbar_links/about.html')
+def safety(request):
+    return render(request,'navbar_links/safety.html')
+def support(request):
+    return render(request,'navbar_links/support.html')
+
+
+#These are the main webpages of the studybud
+def home(request):
+    return render(request,'home.html')
+
+def room(request):
+    return render(request,'room.html')
+
+
