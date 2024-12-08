aad5f1882e4c3208d9841212c8c91f10ed411c1b
vinay joshi <vinay@vinays-MacBook-Air.local>
2024-12-08 01:17:29 -0500

    made changes to the create room page

--- base/forms.py
+++ base/forms.py
@@ -1,11 +1,20 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Room, Message
 
 class RoomForm(ModelForm):
+    topic = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'placeholder': 'Enter a tag',
+        'class': 'form-control'
+    }))
+
     class Meta:
         model = Room
-        fields = '__all__'
-        exclude =['host','participants']
+        fields = [ 'name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter room title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 6, 'placeholder': 'room description'}),
+        }
 
 
 class MessageForm(ModelForm):
